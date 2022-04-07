f9bf193f0ce39d944b8209f27446004f0ca66753
NaiSaratxaga <nsaratxaga@gmail.com>
2022-04-07 22:54:14 +0200

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//url https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+
+// selecciono el elemento del HTML
+const listDrinks = document.querySelector('.js-list-drinks');
+const urlServer = www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+
+
+// funciones
+
+// declarar los eventos
+
+// peticion al servidor fetch
+
+// LocalStorage
+
+// 1- start app -- Cuando carga la pagina
+//getLocalStorage();//
